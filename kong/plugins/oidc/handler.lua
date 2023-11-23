dd820d61504102618aee8c5004aaf555684f0acd
Raffaele Disabato <raffaele.disabato@cybercore.it>
2023-11-23 09:39:47 +0100

    implemented fix at https://docs.konghq.com/gateway/latest/plugin-development/custom-logic/#migrating-from-baseplugin-module

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -1,5 +1,9 @@
-local BasePlugin = require "kong.plugins.base_plugin"
-local OidcHandler = BasePlugin:extend()
+--local BasePlugin = require "kong.plugins.base_plugin"
+--local OidcHandler = BasePlugin:extend()
+local OidcHandler = {
+  VERSION  = "1.0.0",
+  PRIORITY = 10,
+}
 local utils = require("kong.plugins.oidc.utils")
 local filter = require("kong.plugins.oidc.filter")
 local session = require("kong.plugins.oidc.session")
@@ -7,12 +11,14 @@ local session = require("kong.plugins.oidc.session")
 OidcHandler.PRIORITY = 1000
 
 
+--[[
 function OidcHandler:new()
   OidcHandler.super.new(self, "oidc")
 end
+--]]
 
 function OidcHandler:access(config)
-  OidcHandler.super.access(self)
+  --OidcHandler.super.access(self)
   local oidcConfig = utils.get_options(config, ngx)
 
   if filter.shouldProcessRequest(oidcConfig) then
